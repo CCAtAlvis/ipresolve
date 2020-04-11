23e1767d282acb8437353fb3d924efc5c6591e70
Chinmay Chandak <chandakchinmay@gmail.com>
2020-04-11 16:16:12 +0530

    demo

--- relay-server.js
+++ relay-server.js
@@ -5,51 +5,79 @@ const hostname = 'localhost';
 const port = 8080;
 
 let socket;
+let requestId = 0;
 
 const httpServer = (req, res) => {
   console.log("Incomming request");
 
   const keys = [
-// "readable",
-// "socket",
-// "connection",
-"httpVersionMajor",
-"httpVersionMinor",
-"httpVersion",
-"complete",
-"headers",
-"rawHeaders",
-"trailers",
-"rawTrailers",
-"aborted",
-"upgrade",
-"url",
-"method",
-"statusCode",
-"statusMessage",
-// "client",
+    // "readable",
+    // "socket",
+    // "connection",
+    // "httpVersionMajor",
+    // "httpVersionMinor",
+    // "httpVersion",
+    // "complete",
+    "headers",
+    // "rawHeaders",
+    // "trailers",
+    // "rawTrailers",
+    // "aborted",
+    // "upgrade",
+    "url",
+    "method",
+    // "statusCode",
+    // "statusMessage",
+    // "client",
   ];
 
   let request = {};
   for (let i in keys) {
     const key = keys[i];
 
-    request[key] = res[key];
+    request[key] = req[key];
+    // console.log(key, ":", req[key]);
   }
+  request.type = 'request';
+  request.reqId = requestId;
+  requestId++;
+  // console.log({ request });
 
   socket.send(JSON.stringify(request));
 
   socket.on('message', (msg) => {
-    res.end(msg);
+    try {
+      const incomingRes = JSON.parse(msg);
+      res.statusCode = incomingRes.statusCode;
+      res.statusMessage = incomingRes.statusMessage;
+
+      for (let k in incomingRes.headers) {
+        // console.log(k, incomingRes.headers[k]);
+        res.setHeader(k, incomingRes.headers[k]);
+      }
+
+      res.end(incomingRes.data);
+      res = null;
+    } catch (err) {
+      console.log(err);
+    }
   });
 };
 
 const server = http.createServer(httpServer);
-const wss = new WebSocket.Server({'server': server});
+const wss = new WebSocket.Server({ 'server': server });
+
+let clientId = 0;
 
 wss.on('connection', (ws) => {
+  console.log('client connected');
   socket = ws;
+
+  ws.send(JSON.stringify({ clientId: clientId }));
+  clientId++;
+
+  // ws.on('message', (msg) => console.log(msg));
 });
 
 
-server.listen(port);
+server.listen(port, hostname);
